fc5618560e389029a52c2075b12d12e0850aa084
WhiteBoots <2077797162@qq.com>
2022-10-14 23:21:26 +0800

    Finsh project

--- app/src/main/java/com/yitu/pictureshare/IndexActivity.java
+++ app/src/main/java/com/yitu/pictureshare/IndexActivity.java
@@ -8,13 +8,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -59,8 +57,6 @@ public class IndexActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.show();
 
         super.onCreate(savedInstanceState);
 
@@ -72,11 +68,8 @@ public class IndexActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_profile)
-                .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
         if (ContextCompat.checkSelfPermission(this,
---
+++ app/src/main/java/com/yitu/pictureshare/common/AppAuthorization.java
@@ -0,0 +1,14 @@
+package com.yitu.pictureshare.common;
+
+import android.content.SharedPreferences;
+
+public class AppAuthorization {
+
+    public static String getAppId(SharedPreferences sp) {
+        return sp.getString("appId",null);
+    }
+
+    public static String getAppSecret(SharedPreferences sp) {
+        return sp.getString("appSecret",null);
+    }
+}
